600621e36fb5608d26edf1e3047c6d4006935873
delcadead <francisdelca@hotmail.com>
2019-04-06 12:45:22 -0500

    Update leeSerial.py

--- leeSerial.py
+++ leeSerial.py
@@ -4,6 +4,7 @@ arduino = Serial('/dev/ttyACM0',9600, timeout = 1)
 while True:
     try:
         a = arduino.readline() #se lee lo que llega por el serial
+        #v = '' +str(int(a))
         file = open('sensor.txt','w') # cracion del archivo que guarda el valor
         file.write(a)
         file.close()
